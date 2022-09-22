48935b50ad4c53a6529039de2f46609bb6d7018e
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-09-23 00:05:03 +0200

    Update movieDB.tsx

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const movieDB = axios.create({
   baseURL: "https://api.themoviedb.org/3/movie",
   params: {
-    api_key: "9c8e29838b2255d4a9671c6105e2f658",
+    api_key: "",
     language: "es-ES",
   },
 });
